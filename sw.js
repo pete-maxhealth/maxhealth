dbcbf346b29d955f8371206e63a08e333ad44e52
pete-maxhealth <your@email.com>
2026-05-14 00:42:47 +0100

    Cache buster: service worker clears all caches and unregisters itself

---
+++ sw.js
@@ -0,0 +1,21 @@
+// MaxedHealth Service Worker - CACHE BUSTER
+// This version immediately clears all caches and unregisters itself
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
